db54ac76d02c5ab09d3cab80615da725ef09f49e
Roman <Roman.Mettler@gmail.com>
2020-09-14 09:22:57 -0300

    crea esqueleto de controladores

--- models/genre.js
+++ models/genre.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+var mongoose = require('mongoose');
 
-const GenreSchema = new Schema(
-    {
-        name: { type: String, minlength: 3, maxlength: 100 }
-    }
-)
+var Schema = mongoose.Schema;
 
+var GenreSchema = new Schema({
+    name: {type: String, required: true, min: 3, max: 100}
+});
+
+// Virtual for this genre instance URL.
 GenreSchema
-    .virtual('url')
-    .get(() => {
-        return '/catalog/genre/' + this._id
-    })
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/'+this._id;
+});
+
+// Export model.
+module.exports = mongoose.model('Genre', GenreSchema);
